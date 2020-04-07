03cde2471c3a4614b3c5289df2b7dbd7a7f4862c
Renan <renan_velox@hotmail.com>
2020-04-07 15:31:33 -0300

    Criando as funcionalidades de atualizar e deletar o entregador

--- src/app/controllers/DeliverymanController.js
+++ src/app/controllers/DeliverymanController.js
@@ -1,6 +1,7 @@
 const yup = require('yup');
 
 const DeliveryMan = require('../models/DeliveryMan');
+const File = require('../models/File');
 
 class DeliverymanController {
    async index(req, res) {
@@ -37,6 +38,65 @@ class DeliverymanController {
       return res.json({ 
          id, name, email, avatar_id,
       });
+   }
+
+   async update(req, res) {
+      const { id } = req.params;
+      const { name, email, avatar_id } = req.body;
+
+
+      if (avatar_id) { 
+         const avatarExists = await File.findByPk(avatar_id);
+
+         if (!avatarExists) {
+            return res.status(401).json({ erro: 'Avatar does is not exists' });
+         }
+      }
+
+      const deliveryMan = await DeliveryMan.findByPk(id);
+
+      if (!deliveryMan) {
+         return res.status(401).json({ erro: 'Deliveryman does not exists' });
+      }
+
+
+      if (email !== DeliveryMan.email) {
+         const deliveryExist = await DeliveryMan.findOne({ where: { email: req.body.email } });
+         
+         if (deliveryExist) {
+            return res.status(400).json({ error: 'User already exists' });
+         }
+      }
+
+
+      await deliveryMan.update({ id, email, avatar_id });
+
+      const { avatar } = await DeliveryMan.findByPk(id, {
+         include: [{
+            model: File,
+            as: 'avatar',
+            attributes: ['id', 'path', 'url'],
+         }],
+      });
+     
+
+      return res.json({ 
+         id, name, email, avatar, 
+      });
+   }
+
+   async delete(req, res) {
+      const { id } = req.params;
+
+      const deliveryMan = await DeliveryMan.findByPk(id);
+
+      if (!deliveryMan) {
+         return res.status(401).json({ erro: 'Deliveryman does not exists' });
+      }
+
+      await deliveryMan.destroy();
+
+      return res.json({});
    }
 }
 
